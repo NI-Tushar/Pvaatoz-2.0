bcf0c6147f447c82c5ee589f012cd40eb49eb46b
NI Tushar <nazrulislamtusar@gmail.com>
2026-01-06 03:56:05 +0600

    features three cards for landing page developed

--- resources/views/Pages/home.blade.php
+++ resources/views/Pages/home.blade.php
@@ -456,23 +456,29 @@
       <div class="container">
         <div class="row">
 
-          <div class="col-lg-4 box">
-            <i class="bi bi-briefcase"></i>
-            <h4 class="title"><a href="">Lorem Ipsum Delino</a></h4>
-            <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
-          </div>
+            <div class="col-lg-4 box">
+                <img class="h-[60px] w-auto" src="{{ asset('Frontend/icons/text_icon/genuine.png') }}" alt="">
+                <h4 class="title"><a href="">Genuine PVA Accounts</a></h4>
+                <p class="description">
+                    We provide 100% genuine and verified PVA accounts created with real information to ensure safety, stability, and long-term usability.
+                </p>
+            </div>
 
-          <div class="col-lg-4 box box-bg">
-            <i class="bi bi-card-checklist"></i>
-            <h4 class="title"><a href="">Dolor Sitema</a></h4>
-            <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
-          </div>
+            <div class="col-lg-4 box box-bg">
+                <img class="h-[60px] w-auto" src="{{ asset('Frontend/icons/text_icon/gift-box.png') }}" alt="">
+                <h4 class="title"><a href="">Multiple Packages</a></h4>
+                <p class="description">
+                    Choose from a variety of flexible PVA packages designed to meet different business and marketing needs at competitive prices.
+                </p>
+            </div>
 
-          <div class="col-lg-4 box">
-            <i class="bi bi-binoculars"></i>
-            <h4 class="title"><a href="">Sed ut perspiciatis</a></h4>
-            <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-          </div>
+            <div class="col-lg-4 box">
+                <img class="h-[60px] w-auto" src="{{ asset('Frontend/icons/text_icon/cash-on-delivery.png') }}" alt="">
+                <h4 class="title"><a href="">Fast & Secure Delivery</a></h4>
+                <p class="description">
+                    All orders are processed quickly with secure delivery, ensuring you receive high-quality accounts without unnecessary delays.
+                </p>
+            </div>
 
         </div>
       </div>
